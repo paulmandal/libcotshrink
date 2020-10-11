01342440e9243a6288dbac6f964ae515e84b9576
Paul Mandal <paul.mandal@gmail.com>
2020-10-11 12:14:26 -0600

    Add initial library skeleton

---
+++ src/main/java/com/paulmandal/atak/libcotshrink/api/CotShrinker.java
@@ -0,0 +1,16 @@
+package com.paulmandal.atak.libcotshrink.api;
+
+import com.atakmap.coremap.cot.event.CotEvent;
+
+public class CotShrinker {
+
+    public byte[] cotEventToBytes(CotEvent cotEvent) {
+        // TODO: impl
+        return null;
+    }
+
+    public CotEvent cotEventFromBytes(byte[] cotEventBytes) {
+        // TODO: impl
+        return null;
+    }
+}
