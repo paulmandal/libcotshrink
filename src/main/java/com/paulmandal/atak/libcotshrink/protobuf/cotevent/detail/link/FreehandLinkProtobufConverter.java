f24338e39b155a2b3d83b18a01d89743311469da
Paul Mandal <paul.mandal@gmail.com>
2021-11-07 13:14:09 -0700

    Repackaged all Protobuf Converters to fit with the CotEvent XML hierarchy

---
+++ src/main/java/com/paulmandal/atak/libcotshrink/protobuf/cotevent/detail/link/FreehandLinkProtobufConverter.java
@@ -0,0 +1,54 @@
+package com.paulmandal.atak.libcotshrink.protobuf.cotevent.detail.link;
+
+import com.atakmap.coremap.cot.event.CotAttribute;
+import com.atakmap.coremap.cot.event.CotDetail;
+import com.google.protobuf.ByteString;
+import com.paulmandal.atak.libcotshrink.gzip.GzipHelper;
+import com.paulmandal.atak.libcotshrink.protobuf.exceptions.UnknownDetailFieldException;
+import com.paulmandal.atak.libcotshrink.protobufs.ProtobufFreehandLink;
+
+import java.io.IOException;
+
+public class FreehandLinkProtobufConverter {
+    private static final String KEY_LINK = "link";
+
+    private static final String KEY_LINE = "line";
+
+    public ProtobufFreehandLink.FreehandLink toFreehandLink(CotDetail cotDetail) throws UnknownDetailFieldException {
+        ProtobufFreehandLink.FreehandLink.Builder builder = ProtobufFreehandLink.FreehandLink.newBuilder();
+        CotAttribute[] attributes = cotDetail.getAttributes();
+        for (CotAttribute attribute : attributes) {
+            switch (attribute.getName()) {
+                case KEY_LINE:
+                    try {
+                        builder.setLine(ByteString.copyFrom(GzipHelper.compress(attribute.getValue()))); // TODO: evaluate this using EXI instead
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    throw new UnknownDetailFieldException("Don't know how to handle detail field: freehandlink." + attribute.getName());
+            }
+        }
+        return builder.build();
+    }
+
+    public void maybeAddFreehandLink(CotDetail cotDetail, ProtobufFreehandLink.FreehandLink freehandLink) {
+        if (freehandLink == null || freehandLink == ProtobufFreehandLink.FreehandLink.getDefaultInstance()) {
+            return;
+        }
+
+        CotDetail freehandLinkDetail = new CotDetail(KEY_LINK);
+
+        ByteString freehandLine = freehandLink.getLine();
+        if (freehandLine != ProtobufFreehandLink.FreehandLink.getDefaultInstance().getLine()) {
+            try {
+                freehandLinkDetail.setAttribute(KEY_LINE, GzipHelper.decompress(freehandLine.toByteArray()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        cotDetail.addChild(freehandLinkDetail);
+    }
+}
