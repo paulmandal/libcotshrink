5a5cb2cc62c5063c245d4e55cba470aeda550b63
Paul Mandal <paul.mandal@gmail.com>
2021-11-07 13:27:11 -0700

    Do not attempt to use protobufs for CotEvents without a `type`, assume type is `a-f-G-U-C` if one is not found in the protobuf payload

---
+++ src/main/java/com/paulmandal/atak/libcotshrink/protobuf/exceptions/InvalidCotEventTypeException.java
@@ -0,0 +1,7 @@
+package com.paulmandal.atak.libcotshrink.protobuf.exceptions;
+
+public class InvalidCotEventTypeException extends Exception {
+    public InvalidCotEventTypeException(String message) {
+        super(message);
+    }
+}
